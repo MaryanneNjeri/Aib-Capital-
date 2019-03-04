4de425fe77ea052931a26c7eed31a2a1083a3fee
MaryAnne45 <mary.jereh@gmail.com>
2019-03-04 12:43:02 +0300

    created the query model

---
+++ app/Query.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Query extends Model
+{
+    // 
+    protected $fillable = { 
+        
+        'name','telephone_number','email','location','query'
+    }
+}
