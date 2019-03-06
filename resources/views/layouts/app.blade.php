97c77b71a195d1489c0a108021e04009af651f88
MaryAnne45 <mary.jereh@gmail.com>
2019-03-06 18:57:55 +0300

    designed the web page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Laravel 5.7 CRUD Example Tutorial</title>
+  <title>AIB Capital</title>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>
-  <div class="container">
+  <div class="container-fluid"  style="background-image:url(../images/backimage.jpg);background-repeat: no-repeat; background-attachment: fixed; height:1200px;background-position:center center ;  background-size: cover;">
     @yield('content')
   </div>
   <script src="{{ asset('js/app.js') }}" type="text/js"></script>
 </body>
 </html>
+
--- resources/views/query.blade.php
+++ resources/views/query.blade.php
@@ -1,56 +1,142 @@
-@extends ('layout') 
-@section ('content')
- 
-<style>
-  .uper {
-    margin-top: 40px;
-  }
-</style> 
-@if(session()->get('success'))
+@extends ('layouts.app')
+@section ('content') 
+<style> 
+body, html {
+  height: 100%;
+}
+.card {
+  background-color: orange;
+  border-radius: 25px;
+} 
+.heading {
+  padding: 10px;
+} 
+.content { 
+  width: 600px;
+  position: relative;
+  background-color : rgb(240, 240 , 240);
+  padding: 100px;
+  font-size: 18px;
+  opacity:0.7;
+  font-weight: bold;
+} 
+.jumbotron-fluid {
+  opacity :0.8;
+  height: 200px;
+} 
+.paragraph{
+  padding: 70px;
+} 
+.img {
+  float:right;
+} 
+.col-md-5 {
+  padding-left: 100px;
+} 
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  height: 70px;
+  width: 100%;
+  background-color:black;
+  color: white;
+  text-align: center;
+}
+</style>
+
+   
+
+    @if(session()->get('success'))
     <div class="alert alert-success">
-      {{ session()->get('success') }}  
-    </div><br />
-  @endif
-<div class="card uper"> 
+        {{ session()->get('success') }}
+    </div><br/>
+    @endif 
+    <br>
+    <br>
+   <div class="jumbotron jumbotron-fluid"> 
+    <div class="row">
+    <div class="col-md-5">
+    <img src="images/aib_logo.png" height="120" width="200">
+    </div>
+    <div class="col-md-7">
+    <br>
+    <h3><b>Think Investment, Think *543#</b></h3>
+    </div>
+    </div>
 
-<div class="card-header">
-<h4>AIB Capital</h4> 
-</div> 
-<div class="card-body">
-    @if ($errors->any())
-      <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-      </div><br /> 
-      @endif  
-      <form method="post" action="{{url ('/create/query') }}">
-      <div class="form-group">
-      @csrf 
-      <label for="name">Your Name </label>
-      <input type="text" class="form-control" name="your_name"/>
-      </div>
+   </div>
+    <div class="row">
+        <div class="col-md-8"> 
+        
+        
+        <div class="paragraph">
+        <p class="content">
+      The AIB Auto Account Opening Solution offers you the <br>
 
-      <div class="form-group">
-      <label for="number">Telephone Number </label>
-      <input type="text" class="form-control" name="telephone_number"/>
-      </div> 
-      <div class="form-group">
-      <label for="email">Email </label>
-      <input type="email" class="form-control" name="email"/>
-      </div> 
-      <div class="form-group">
-      <label for="location"> Location</label>
-      <input type="text" class="form-control" name="location"/>
-      </div> 
-      <div class="form-group">
-      <label for="query"> Query</label>
-      <textarea class="form-control" name="query"></textarea>
-      </div> 
-      <button type="submit" class="btn btn-success">Submit</button> 
-      </form>
-      </div> 
-      </div>
-      @endsection
+      <br>opportunity to invest in the Nairobi Securities Exchange <br>
+
+     <br>straight from your mobile phone or computer.<br>
+
+      <br>Sign up today and discover new possibilities! <br>
+
+      <br>Sign up today,log on to  <a href="https://onlinetrading.nse.co.ke/TradeWeb101/login.aspx<">https://onlinetrading.nse.co.ke/TradeWeb101/login.aspx</a>
+        </P> 
+        </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card"> 
+            <br>
+            <p class="heading"><b>Please fill in the query in 
+            the box below and we will get back to you
+            </b></p>
+              
+                <div class="card-body">
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div><br />
+                    @endif
+                    <form method="post" action="{{url ('/create/query') }}">
+                        <div class="form-group">
+                            @csrf
+                            <label for="name">Your Name </label>
+                            <input type="text" class="form-control" name="your_name" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="number">Telephone Number </label>
+                            <input type="text" class="form-control" name="telephone_number" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email </label>
+                            <input type="email" class="form-control" name="email" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="location"> Location</label>
+                            <input type="text" class="form-control" name="location" />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="query"> Query</label>
+                            <textarea class="form-control" name="query"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-warning">Submit</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div> 
+    <div class="footer">
+  <p>@Copyright <a style="color:orange;" href="http://www.aibcapital.com/">aibcapital.com</a> 2019.All rights reserved</p>
+</div>
+
+@endsection
